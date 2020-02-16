010b933158684471ed733938d76372aed41aca15
keer2345 <keer2345@gmail.com>
2020-02-16 18:48:44 +0800

    clojure

--- lisp/lang-clojure.el
+++ lisp/lang-clojure.el
@@ -11,22 +11,24 @@
              (progn
                (setq clojure-align-forms-automatically t)))
 
-(use-package cider
-             :init
-             (add-hook 'cider-mode-hook #'clj-refactor-mode)
-             (add-hook 'cider-mode-hook #'company-mode)
-             (add-hook 'cider-mode-hook #'eldoc-mode)
-             (add-hook 'cider-repl-mode-hook #'company-mode)
-             (add-hook 'cider-repl-mode-hook #'eldoc-mode)
-             :diminish subword-mode
-             :config
-             (setq nrepl-log-messages t
-                   cider-repl-display-in-current-window t
-                   cider-repl-use-clojure-font-lock t
-                   cider-prompt-save-file-on-load 'always-save
-                   cider-font-lock-dynamically '(macro core function var)
-                   cider-overlays-use-font-lock t)
-             (cider-repl-toggle-pretty-printing))
+; (use-package cider
+             ; :init
+             ; (add-hook 'cider-mode-hook #'clj-refactor-mode)
+             ; (add-hook 'cider-mode-hook #'company-mode)
+             ; (add-hook 'cider-mode-hook #'eldoc-mode)
+             ; (add-hook 'cider-repl-mode-hook #'company-mode)
+             ; (add-hook 'cider-repl-mode-hook #'eldoc-mode)
+             ; :diminish subword-mode
+             ; :config
+             ; (setq nrepl-log-messages t
+                   ; cider-repl-display-in-current-window t
+                   ; cider-repl-use-clojure-font-lock t
+                   ; cider-prompt-save-file-on-load 'always-save
+                   ; cider-font-lock-dynamically '(macro core function var)
+                   ; cider-overlays-use-font-lock t)
+             ; (cider-repl-toggle-pretty-printing))
+
+(use-package cider)
 
 (use-package clj-refactor
              :diminish clj-refactor-mode
